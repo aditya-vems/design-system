134c1786e02151effb06f8302a53d2bb41876d06
Aditya Patil <outofaditya@gmail.com>
2026-04-19 14:08:24 +0200

    fix: format source documents

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,278 @@
-export { Button, buttonVariants } from './components/inputs/button';
-export { ButtonGroup } from './components/inputs/button-group';
-export { Calendar } from './components/inputs/calendar';
-export { Checkbox } from './components/inputs/checkbox';
-export { Combobox } from './components/inputs/combobox';
-export { Field, FieldGroup, FieldLabel } from './components/inputs/field';
-export { Input } from './components/inputs/input';
-export { InputGroup } from './components/inputs/input-group';
-export { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator } from './components/inputs/input-otp';
-export { Label } from './components/inputs/label';
-export { NativeSelect } from './components/inputs/native-select';
-export { RadioGroup, RadioGroupItem } from './components/inputs/radio-group';
-export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectLabel, SelectItem, SelectSeparator, SelectScrollUpButton, SelectScrollDownButton } from './components/inputs/select';
-export { Slider } from './components/inputs/slider';
-export { Switch } from './components/inputs/switch';
-export { Textarea } from './components/inputs/textarea';
-export { Toggle, toggleVariants } from './components/inputs/toggle';
-export { ToggleGroup } from './components/inputs/toggle-group';
+import "./styles/globals.css";
+export { cn } from "@/helpers/utils";
+export { useIsMobile } from "@/hooks/use-mobile";
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './components/data-display/accordion';
-export { AspectRatio } from './components/data-display/aspect-ratio';
-export { Avatar, AvatarImage, AvatarFallback } from './components/data-display/avatar';
-export { Badge } from './components/data-display/badge';
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './components/data-display/card';
-export { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from './components/data-display/carousel';
-export { ChartContainer, ChartTooltip, ChartTooltipContent, ChartLegend, ChartLegendContent, ChartStyle } from './components/data-display/chart';
-export type { ChartConfig } from './components/data-display/chart';
-export { Item } from './components/data-display/item';
-export { Kbd } from './components/data-display/kbd';
-export { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption } from './components/data-display/table';
-export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/data-display/tabs';
+export {
+  Select,
+  SelectItem,
+  SelectLabel,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "@/components/inputs/select";
+export {
+  InputOTP,
+  InputOTPSlot,
+  InputOTPGroup,
+  InputOTPSeparator,
+} from "@/components/inputs/input-otp";
+export {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "@/components/data-display/tabs";
+export {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardFooter,
+  CardContent,
+  CardDescription,
+} from "@/components/data-display/card";
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/data-display/accordion";
+export {
+  Carousel,
+  CarouselItem,
+  CarouselNext,
+  CarouselContent,
+  CarouselPrevious,
+} from "@/components/data-display/carousel";
+export {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "@/components/data-display/avatar";
+export {
+  ChartStyle,
+  ChartLegend,
+  ChartTooltip,
+  ChartContainer,
+  ChartTooltipContent,
+  ChartLegendContent,
+} from "@/components/data-display/chart";
+export {
+  Table,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableFooter,
+  TableCaption,
+} from "@/components/data-display/table";
+export {
+  Alert,
+  AlertTitle,
+  AlertDescription,
+} from "@/components/feedback/alert";
+export {
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogCancel,
+  AlertDialogHeader,
+  AlertDialogAction,
+  AlertDialogFooter,
+  AlertDialogContent,
+  AlertDialogTrigger,
+  AlertDialogDescription,
+} from "@/components/feedback/alert-dialog";
+export {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@/components/layout/collapsible";
+export {
+  ResizablePanel,
+  ResizableHandle,
+  ResizablePanelGroup,
+} from "@/components/layout/resizable";
+export {
+  Sheet,
+  SheetClose,
+  SheetTitle,
+  SheetHeader,
+  SheetFooter,
+  SheetTrigger,
+  SheetContent,
+  SheetDescription,
+} from "@/components/overlay/sheet";
+export {
+  Drawer,
+  DrawerClose,
+  DrawerTitle,
+  DrawerHeader,
+  DrawerFooter,
+  DrawerTrigger,
+  DrawerContent,
+  DrawerDescription,
+} from "@/components/overlay/drawer";
+export {
+  Dialog,
+  DialogClose,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+} from "@/components/overlay/dialog";
+export {
+  Command,
+  CommandList,
+  CommandItem,
+  CommandEmpty,
+  CommandInput,
+  CommandGroup,
+  CommandDialog,
+  CommandShortcut,
+  CommandSeparator,
+} from "@/components/overlay/command";
+export {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from "@/components/overlay/hover-card";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/overlay/tooltip";
+export {
+  Popover,
+  PopoverAnchor,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/overlay/popover";
+export {
+  DropdownMenu,
+  DropdownMenuSub,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuShortcut,
+  DropdownMenuSeparator,
+  DropdownMenuRadioItem,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+  DropdownMenuCheckboxItem,
+} from "@/components/overlay/dropdown-menu";
+export {
+  ContextMenu,
+  ContextMenuSub,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuTrigger,
+  ContextMenuContent,
+  ContextMenuShortcut,
+  ContextMenuSeparator,
+  ContextMenuRadioItem,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuRadioGroup,
+  ContextMenuCheckboxItem,
+} from "@/components/overlay/context-menu";
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/navigation/breadcrumb";
+export {
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationContent,
+  PaginationPrevious,
+  PaginationEllipsis,
+} from "@/components/navigation/pagination";
+export {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarProvider,
+  SidebarTrigger,
+  SidebarInset,
+  SidebarRail,
+  useSidebar,
+} from "@/components/navigation/sidebar";
+export {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuViewport,
+} from "@/components/navigation/navigation-menu";
+export {
+  Menubar,
+  MenubarSub,
+  MenubarMenu,
+  MenubarItem,
+  MenubarLabel,
+  MenubarGroup,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarShortcut,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarRadioGroup,
+  MenubarCheckboxItem,
+} from "@/components/navigation/menubar";
 
-export { Alert, AlertTitle, AlertDescription } from './components/feedback/alert';
-export { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogFooter, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel } from './components/feedback/alert-dialog';
-export { Empty } from './components/feedback/empty';
-export { Progress } from './components/feedback/progress';
-export { Skeleton } from './components/feedback/skeleton';
-export { Toaster } from './components/feedback/sonner';
-export { Spinner } from './components/feedback/spinner';
-
-export { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from './components/navigation/breadcrumb';
-export { Menubar, MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem, MenubarCheckboxItem, MenubarRadioItem, MenubarLabel, MenubarSeparator, MenubarShortcut, MenubarGroup, MenubarSub, MenubarSubContent, MenubarSubTrigger, MenubarRadioGroup } from './components/navigation/menubar';
-export { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuTrigger, NavigationMenuLink, NavigationMenuContent, NavigationMenuIndicator, NavigationMenuViewport } from './components/navigation/navigation-menu';
-export { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from './components/navigation/pagination';
-export { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarProvider, SidebarTrigger, SidebarInset, SidebarRail, useSidebar } from './components/navigation/sidebar';
-
-export { Command, CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandSeparator, CommandShortcut } from './components/overlay/command';
-export { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuItem, ContextMenuCheckboxItem, ContextMenuRadioItem, ContextMenuLabel, ContextMenuSeparator, ContextMenuShortcut, ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger, ContextMenuRadioGroup } from './components/overlay/context-menu';
-export { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogFooter, DialogTitle, DialogDescription, DialogClose } from './components/overlay/dialog';
-export { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerFooter, DrawerTitle, DrawerDescription, DrawerClose } from './components/overlay/drawer';
-export { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuCheckboxItem, DropdownMenuRadioItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuGroup, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuRadioGroup } from './components/overlay/dropdown-menu';
-export { HoverCard, HoverCardTrigger, HoverCardContent } from './components/overlay/hover-card';
-export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor } from './components/overlay/popover';
-export { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetFooter, SheetTitle, SheetDescription, SheetClose } from './components/overlay/sheet';
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from './components/overlay/tooltip';
-
-export { Collapsible, CollapsibleTrigger, CollapsibleContent } from './components/layout/collapsible';
-export { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './components/layout/resizable';
-export { ScrollArea, ScrollBar } from './components/layout/scroll-area';
-export { Separator } from './components/layout/separator';
-
-export { useIsMobile } from './hooks/use-mobile';
-export { cn } from './helpers/utils';
-
-import './styles/globals.css';
+export { Input } from "@/components/inputs/input";
+export { Label } from "@/components/inputs/label";
+export { Empty } from "@/components/feedback/empty";
+export { Slider } from "@/components/inputs/slider";
+export { Kbd } from "@/components/data-display/kbd";
+export { Switch } from "@/components/inputs/switch";
+export { Item } from "@/components/data-display/item";
+export { Toaster } from "@/components/feedback/sonner";
+export { Combobox } from "@/components/inputs/combobox";
+export { Calendar } from "@/components/inputs/calendar";
+export { Checkbox } from "@/components/inputs/checkbox";
+export { Textarea } from "@/components/inputs/textarea";
+export { Badge } from "@/components/data-display/badge";
+export { Spinner } from "@/components/feedback/spinner";
+export { Progress } from "@/components/feedback/progress";
+export { Skeleton } from "@/components/feedback/skeleton";
+export { Separator } from "@/components/layout/separator";
+export { InputGroup } from "@/components/inputs/input-group";
+export { ToggleGroup } from "@/components/inputs/toggle-group";
+export { ButtonGroup } from "@/components/inputs/button-group";
+export { NativeSelect } from "@/components/inputs/native-select";
+export type { ChartConfig } from "@/components/data-display/chart";
+export { Button, buttonVariants } from "@/components/inputs/button";
+export { Toggle, toggleVariants } from "@/components/inputs/toggle";
+export { AspectRatio } from "@/components/data-display/aspect-ratio";
+export { ScrollArea, ScrollBar } from "@/components/layout/scroll-area";
+export { Field, FieldGroup, FieldLabel } from "@/components/inputs/field";
+export { RadioGroup, RadioGroupItem } from "@/components/inputs/radio-group";
